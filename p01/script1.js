845b05cadfb805ab0eb71a54739d3db89521c9f2
adil-innovation-lab <khanadilaltaf@gmail.com>
2020-08-08 13:07:51 +0500

    archive script1.js and create new script.js
    Refactored required field check

--- p01/script1.js
+++ p01/script1.js
@@ -25,27 +25,33 @@ function isValidEmail(email) {
     return re.test(String(email).toLowerCase());
 }
 
-// Function to check if required fields have data
-function checkRequired(inputArray) {
-    inputArray.forEach(function(input) {
-        if ( input.value === '' ) {
-            console.log(input.id);
-            showError(input,`${getFieldId(input)} is required`);
-        } else {
-            showSuccess(input);
-        }
-    });
-}
-
-// Function to get the id of the input field with proper case
-function getFieldId(input) {
-    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-}
-
 // This is an event listener for the form on submit
 form.addEventListener('submit',function(e) {
     e.preventDefault();
+    
+    if ( username.value === '' ) {
+        showError(username,'Username is required');
+    } else {
+        showSuccess(username);
+    }
 
-    checkRequired([username,email,password,password2]);
+    if ( email.value === '' ) {
+        showError(email,'Email is required');
+    } else if (!isValidEmail(email.value)) {
+        showError(email,'Email is invalid')
+    } else {
+        showSuccess(email);
+    }
 
+    if ( password.value === '' ) {
+        showError(password,'Password is required');
+    } else {
+        showSuccess(password);
+    }
+
+    if ( password2.value === '' ) {
+        showError(password2,'Password2 is required');
+    } else {
+        showSuccess(password2);
+    }
 })
